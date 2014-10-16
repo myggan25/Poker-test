9b51b241e68ec81ab159c0f6071ca535056a5f01
Robin Abrahamsson <robin.abrahamsson25@gmail.com>
2014-10-16 06:34:44 +0200

    It should work now.

--- engine.hs
+++ engine.hs
@@ -7,8 +7,6 @@ data Rank = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten |
           deriving (Show, Eq,  Ord, Enum)
 type Card = (Color, Rank)
 
-type PlayerHands = [[Card]]
-
 createDeck :: [Card]
 createDeck = [(color, number) | number <- [Ace .. King], color <- [Spade .. Club]]
 
@@ -21,10 +19,11 @@ shuffleDeck oldDeck gen = a : shuffleDeck newList gen
     newList = ys ++ (tail zs)
     a = head zs
 
-assignCardsToPlayers :: [Card] -> Int -> Int -> [[Card]]
-assignCardsToPlayers _ _ 0 = []
-assignCardsToPlayers deck numberOfCards numberOfPlayers =
-  (take numberOfCards deck) : (assignCardsToPlayers (drop numberOfCards deck) numberOfCards (numberOfPlayers -1))
+assignCards :: [Card] -> Int -> Int -> [[Card]]
+assignCards _ _ 0 = []
+assignCards deck numberOfCards numberOfPlayers =
+  (take numberOfCards deck) :
+  (assignCards (drop numberOfCards deck) numberOfCards (numberOfPlayers -1))
 
 getFlop :: [Card] -> [Card]
 getFlop deck = take 3 $ drop 1 deck
@@ -35,13 +34,30 @@ getTurn deck = head $ drop 1 deck
 getRiver :: [Card] -> Card
 getRiver = getTurn
 
+haveFlush :: [Card] -> Bool
+haveFlush cards = findFlushHelp cards 0 0 0 0
+
+findFlushHelp :: [Card] -> Int -> Int -> Int -> Int -> Bool
+findFlushHelp [] _ _ _ _ = False
+findFlushHelp _ 5 _ _ _  = True
+findFlushHelp _ _ 5 _ _  = True
+findFlushHelp _ _ _ 5 _  = True
+findFlushHelp _ _ _ _ 5  = True
+findFlushHelp (x:xs) spade diamond heart club =
+  case (x) of
+    (Spade,_) -> findFlushHelp xs (spade+1) diamond heart club
+    (Diamond,_) -> findFlushHelp xs spade (diamond+1) heart club
+    (Heart,_) -> findFlushHelp xs spade diamond (heart+1) club
+    (Club,_) -> findFlushHelp xs spade diamond heart (club+1)
+
+
 main = do
-  gen <- getStdGen
+  gen <- newStdGen
   playDeck <- newIORef (shuffleDeck createDeck gen)
   let numberOfCards = 2;
       numberOfPlayers = 4;
   temp <- (readIORef playDeck)
-  let playerHands = assignCardsToPlayers temp numberOfCards numberOfPlayers
+  let playerHands = assignCards temp numberOfCards numberOfPlayers
   writeIORef playDeck (drop (numberOfCards*numberOfPlayers) temp)
   
   putStrLn(show playerHands)
@@ -60,3 +76,5 @@ main = do
   let river = getRiver temp
   writeIORef playDeck (drop 2 temp)
   putStrLn ("River: " ++ (show river))
+
+  putStrLn (show (haveFlush ((head playerHands) ++ flop ++ [turn] ++ [river])))
